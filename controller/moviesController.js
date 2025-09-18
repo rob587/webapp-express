3937e4be576ac0a2071f31e17dc6ca1b52ed28be
rob587 <robertocamm02@yahoo.com>
2025-09-18 22:33:41 +0200

    show aggiunto

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -13,7 +13,14 @@ const index = (req, res) =>{
 
 
 const show = (req, res) =>{
-    console.log('show')
+    const {id} = req.params.id
+    
+    const sql = 'SELECT * FROM movies WHERE ID = ?'
+    connection.query(sql, [id], (err,results) =>{
+        if(err) return res.status(500).json({error: 'Query del database fallita'})
+        if(results.length === 0) return res.status(404).json ({error: 'Film non trovato'})
+        res.json(results[0])
+    })
 }
 
 module.exports={
