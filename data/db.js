326e302ce42e28ba3e8c205d30f1b1911eda069f
rob587 <robertocamm02@yahoo.com>
2025-09-18 14:57:24 +0200

    database connesso

--- data/db.js
+++ data/db.js
@@ -1,2 +1,15 @@
 const mysql = require('mysql2')
 
+const connection = mysql.createConnection({
+    "host": 'localhost',
+    "user": 'root',
+    "password": 'root',
+    "database": 'db_movies'
+})
+
+connection.connect((err)=> {
+    if (err) throw err
+    console.log('Connesso al Database')
+})
+
+module.exports = connection
