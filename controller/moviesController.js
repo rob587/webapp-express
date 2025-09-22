6577366fd1d75c9a7d4c3f49a0da36e305eafada
rob587 <robertocamm02@yahoo.com>
2025-09-22 23:43:10 +0200

    add reviews

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -16,10 +16,19 @@ const show = (req, res) =>{
     const id = req.params.id
     
     const sql = 'SELECT * FROM movies WHERE ID = ?'
+    const reviews = 'SELECT * FROM reviews WHERE movie_id = ?'
     connection.query(sql, [id], (err,results) =>{
         if(err) return res.status(500).json({error: 'Query del database fallita'})
         if(results.length === 0) return res.status(404).json ({error: 'Film non trovato'})
-        res.json(results[0])
+            
+            connection.query(reviews, [id], (err, reviewResults)=>{
+                if (err) return res.status(500).json({ error: 'Query delle reviews fallita' });
+                
+            res.json({
+                film:results[0],
+                reviews: reviewResults
+            })
+        })
     })
 }
 
